624aae400039e46686b496ec3b9e32b165d1c8f7
kalyak <kalyakusumadi@gmail.com>
2021-04-06 00:05:08 +0800

    Initial set up of client (create-react-app) and server (npm init) folders.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      Look out for a brand new Recipe Sharing Hub coming soon!
+    </div>
+  );
+}
+
+export default App;
